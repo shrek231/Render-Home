7f8db670344ef00c93c40e6ac09eff2e5132fc7f
Shrek231 <49622239+shrek231@users.noreply.github.com>
2021-01-08 15:28:57 -0800

    still dosent have backend, just a websocket test

--- Program.cs
+++ Program.cs
@@ -1,29 +1,31 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+using System;
 using System.Threading;
 using Websocket.Client;
-
 namespace Render {
     class Program {
         static void Main(string[] args) {
             bool running = true;
             var exitEvent = new ManualResetEvent(false);
             var url = new Uri("wss://echo.websocket.org");
-
             using (var client = new WebsocketClient(url)) {
                 client.ReconnectTimeout = TimeSpan.FromSeconds(30);
-                client.ReconnectionHappened.Subscribe(info =>
-                    Console.WriteLine($"Reconnection happened, type: {info.Type}"));
-
-                client.MessageReceived.Subscribe(msg => {
-                    Console.WriteLine($"Message received: {msg}");
+                client.MessageReceived.Subscribe(msg =>
+                    Console.WriteLine($"Message received: {msg}"));
+                client.DisconnectionHappened.Subscribe(died => {
+                    Console.WriteLine("disconnected for reason " + died);
+                    try {
+                        Console.WriteLine("Trying to reconnect...");
+                        client.Reconnect(); 
+                    }
+                    catch (Exception e) {
+                        Console.WriteLine(e);
+                        throw;
+                    }
                 });
                 client.Start();
                 while (running) {
                     string send = Console.ReadLine();
                     client.Send(send);
-                    client.DisconnectionHappened.Subscribe(died => Console.WriteLine("disconnected for reason "+died));
                 }
                 exitEvent.WaitOne();
             }
