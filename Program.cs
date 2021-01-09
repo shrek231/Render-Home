e6ce4eded0f6b38e01f1b2879f03319c03b60377
Shrek231 <49622239+shrek231@users.noreply.github.com>
2021-01-08 16:41:32 -0800

    simple solution, just use rest

--- Program.cs
+++ Program.cs
@@ -1,34 +1,15 @@
-using System;
-using System.Threading;
-using Websocket.Client;
+﻿using System;
+using RestSharp;
+
 namespace Render {
     class Program {
         static void Main(string[] args) {
-            bool running = true;
-            var exitEvent = new ManualResetEvent(false);
-            var url = new Uri("wss://echo.websocket.org");
-            using (var client = new WebsocketClient(url)) {
-                client.ReconnectTimeout = TimeSpan.FromSeconds(30);
-                client.MessageReceived.Subscribe(msg =>
-                    Console.WriteLine($"Message received: {msg}"));
-                client.DisconnectionHappened.Subscribe(died => {
-                    Console.WriteLine("disconnected for reason " + died);
-                    try {
-                        Console.WriteLine("Trying to reconnect...");
-                        client.Reconnect(); 
-                    }
-                    catch (Exception e) {
-                        Console.WriteLine(e);
-                        throw;
-                    }
-                });
-                client.Start();
-                while (running) {
-                    string send = Console.ReadLine();
-                    client.Send(send);
-                }
-                exitEvent.WaitOne();
-            }
+            //init restsharp
+            var client = new RestClient("https://shranime.org");
+            var request = new RestRequest(Method.POST);
+            IRestResponse response = client.Execute(request); //call at anytime in code
+            
+            Console.WriteLine(response.Content.ToString());
         }
     }
 }
