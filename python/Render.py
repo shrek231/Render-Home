618610aacba5d924a4d7336382f413f4bb1faa12
Shrek231 <49622239+shrek231@users.noreply.github.com>
2021-01-11 03:51:31 -0800

    output is a var now

--- python/Render.py
+++ python/Render.py
@@ -5,8 +5,9 @@ import json
 import sys
 import os
 
-blenderpath = "\"C:\\Program Files\\Blender Foundation\\Blender 2.91\\blender.exe\""
-filepath = "C:\\Users\\Owner\\Desktop\\"
+blenderpath = "blender"
+filepath = "/Desktop/blend/"
+outputpath = "/Desktop/op/"
 
 frame = None
 try:
@@ -15,7 +16,7 @@ try:
         if frame != -1:
             print(f"Starting frame {frame}")
             os.system(f"curl -o {filepath}render.blend https://blenderrenderserver.youtubeadminist.repl.co/getBlend")
-            os.system(f"{blenderpath} -b {filepath}render.blend  -o C:\\tmp\\ -f {frame}")
+            os.system(f"{blenderpath} -b {filepath}render.blend  -o {outputpath} -f {frame}")
             os.system(f"curl -F {frame}=@C:\\tmp\\{frame:04}.png blenderrenderserver.youtubeadminist.repl.co/sendFrame")
             frame = None
             print(f"\nRendered frame {frame}")
